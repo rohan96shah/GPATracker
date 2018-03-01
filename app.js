7490fbee0c554ffe13f948c8523b9521972c45b2
Vaishanavi <vaish.harrypotter295@gmail.com>
2018-03-01 00:12:00 -0600

    updated html and css

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-MyApp = (function(){
+const c1 = "#button1";
+const c2 = "#button2";
+const c3 = "#button3";
+const c4 = "#button4";
 
 
-app.init = function() {
 
-		console.log("start");
-		this.render();
-}
+$(document).ready(function(){
 
-	return app;
-
-})();
+	document.querySelector(c1).onclick = function(){
+		
+    let mtx1_row = document.querySelector(t1).value;
+    let mtx1_col =document.querySelector(t2).value;
+    
+    let mtx2_row =document.querySelector(t3).value;
+    let mtx2_col =  document.querySelector(t4).value;
+   
+    if(mtx1_row )
+    checkCompatibility(mtx1_row, mtx1_col, mtx2_row, mtx2_col);                    
+	}
+});
+	
